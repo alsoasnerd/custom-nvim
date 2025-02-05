4f9379412ab77d631866c748e03ee883e4db590e
alsoasnerd <alsoasnerd@gmail.com>
2025-02-04 22:58:33 -0300

    feat(keybinds): add new keybinds for default vim experience

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,6 +13,26 @@ vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gP", ":Git pull --rebase<CR>")
 vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
 vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
-vim.keymap.set("n", "<leader>gr", ":Git reset<CR>")
-vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
-vim.keymap.set("n", "<leader>gca", ":Git commit --amend<CR>")
+
+-- delete without yanking
+vim.keymap.set("x", "<leader>P", [["_dP]])
+
+--- behave like other capitals
+vim.keymap.set("n", "Y", "y$")
+
+--- keeping it centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- make file executable
+vim.keymap.set("n", "<C-x>", "<cmd>!chmod +x %<CR>", { silent = true })
+
+--- moving text
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- open tmux sessionizer (like a real chad)
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tms<CR>")
